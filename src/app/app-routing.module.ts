996a96df663a8261b8cbfec294f595e6ad93fe94
carloscoronaba <carlos.coronaba@gmail.com>
2024-08-02 13:25:17 -0600

    Estructura de proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthLayoutComponent } from './auth/layouts/auth-layout/auth-layout.component';
+import { LoginPageComponent } from './auth/pages/login-page/login-page.component';
+import { RegisterPageComponent } from './auth/pages/register-page/register-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'auth',
+  loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+},{
+  path: 'dashboard',
+  loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
+},{
+  path:'**',
+  redirectTo: 'auth'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
